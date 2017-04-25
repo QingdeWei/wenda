b36f8ab8884aae5d3ab9c62b649ecb378261c531
weiqingde <weiqingde111@gmail.com>
2017-04-25 10:09:21 +0800

    实现了关键词过滤功能，使用了字典树

--- src/main/java/com/nowcoder/service/QuestionService.java
+++ src/main/java/com/nowcoder/service/QuestionService.java
@@ -4,6 +4,7 @@ import com.nowcoder.dao.QuestionDAO;
 import com.nowcoder.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -14,10 +15,19 @@ import java.util.List;
 public class    QuestionService {
     @Autowired
     QuestionDAO questionDAO;
-
+    @Autowired
+    SensitiveService sensitiveService;
     public int addQuestion(Question question){
         //questionDAO.addQuestion(question);
+
+
+        question.setContent(HtmlUtils.htmlEscape(question.getContent()));
+        question.setTitle(HtmlUtils.htmlEscape(question.getTitle()));
         //敏感词过滤
+        question.setTitle(sensitiveService.filter(question.getTitle()));
+        question.setContent(sensitiveService.filter(question.getContent()));
+
+
         return questionDAO.addQuestion(question)>0 ? question.getId():0;
 
     }
---
+++ src/main/java/com/nowcoder/service/SensitiveService.java
@@ -0,0 +1,145 @@
+package com.nowcoder.service;
+
+import org.apache.commons.lang.CharUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by ZGH on 2017/4/24.
+ */
+@Service
+public class SensitiveService implements InitializingBean{
+
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveService.class);
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        rootNode = new TrieNode();
+        try{
+            InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("SensitiveWords.txt");
+            InputStreamReader read = new InputStreamReader(is);
+            BufferedReader bufferedReader = new BufferedReader(read);
+            String lineTxt;
+            while ((lineTxt = bufferedReader.readLine()) != null){
+                addWord(lineTxt.trim());
+            }
+            read.close();
+
+        }catch (Exception e){
+
+            logger.error("添加敏感词失败",e.getMessage());
+        }
+    }
+
+    private class TrieNode{
+        //
+        private boolean end = false;
+        //
+        Map<Character, TrieNode> subNodes = new HashMap<Character, TrieNode>();
+
+        public void addSubNode(Character key, TrieNode node){
+            subNodes.put(key, node);
+
+        }
+        TrieNode getSubNode(Character key){
+            return subNodes.get(key);
+        }
+
+        boolean isKeyWordEnd(){
+            return end;
+        }
+
+        void setKeyWordEnd(boolean end){
+            this.end = end;
+        }
+
+    }
+
+    //增加关键词
+    private void addWord(String lineTxt){
+        TrieNode tmpNode = rootNode;
+
+        for (int i=0; i<lineTxt.length(); ++i){
+            Character c = lineTxt.charAt(i);
+            TrieNode node = tmpNode.getSubNode(c);
+            if (node == null){
+                node = new TrieNode();
+                tmpNode.addSubNode(c, node);
+            }
+            tmpNode = node;
+
+            if (i == lineTxt.length()-1){
+                tmpNode.setKeyWordEnd(true);
+            }
+        }
+    }
+
+    //判断字符
+    public boolean isSymbol(char c){
+        int ic = (int)c;
+        return !CharUtils.isAsciiAlphanumeric(c) && ((ic<0x2E80)||(ic>0x9FFF)) ;
+    }
+
+
+    private TrieNode rootNode = new TrieNode();
+
+    //过滤关键词
+    public String filter(String text){
+        if (StringUtils.isBlank(text)){
+            return text;
+        }
+        StringBuilder result = new StringBuilder();
+        String replacement = "***";
+        TrieNode tmpNode = rootNode;
+        int begin = 0;
+        int position = 0;
+        while (position < text.length()){
+            char c = text.charAt(position);
+            if (isSymbol(c)){
+                if (tmpNode == rootNode) {
+                    result.append(c);
+                    ++begin;
+                }
+                ++position;
+                continue;
+            }
+            tmpNode = tmpNode.getSubNode(c);
+            if (tmpNode == null){
+                result.append(text.charAt(position));
+                position = position+1;
+                begin = position;
+                tmpNode = rootNode;
+            }else if (tmpNode.isKeyWordEnd()){
+                result.append(replacement);
+                position = position+1;
+                begin = position;
+                tmpNode = rootNode;
+            }else {
+                ++position;
+            }
+        }
+        result.append(text.substring(begin));
+        return result.toString();
+    }
+
+    public static void main(String[] args){
+       SensitiveService  s = new SensitiveService();
+        s.addWord("色情");
+        s.addWord("暴力");
+        s.addWord("赌博");
+        System.out.println(s.filter("你好色^&情").toString());
+
+    }
+
+
+}
